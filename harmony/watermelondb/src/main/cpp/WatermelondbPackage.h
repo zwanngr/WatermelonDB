73dda7cfa11aa79f45c5ff5776c4f96e602be60f
王清照 <w17688746224@163.com>
2025-12-11 14:55:37 +0800

    database 监听方法补充

--- harmony/watermelondb/src/main/cpp/WatermelondbPackage.h
+++ harmony/watermelondb/src/main/cpp/WatermelondbPackage.h
@@ -24,11 +24,30 @@
 
 #pragma once
 
+#include "RNOH/Package.h"
+#include "RNWMDatabaseJSIBridge.h"
 #include "generated/RNOH/generated/BaseWatermelondbPackage.h"
 
 namespace rnoh {
+
+class WatermelondbPackageTurboModuleFactoryDelegate : public TurboModuleFactoryDelegate {
+  public:
+    SharedTurboModule createTurboModule(Context ctx, const std::string &name) const override {
+        if (name == "WMDatabaseJSIBridge") {
+            return std::make_shared<RNWMDatabaseJSIBridge>(ctx, name);
+        }
+        return nullptr;
+    };
+};
+
 class WatermelondbPackage : public BaseWatermelondbPackage {
     using Super = BaseWatermelondbPackage;
     using Super::Super;
+    
+    std::unique_ptr<TurboModuleFactoryDelegate> createTurboModuleFactoryDelegate() override {
+        return std::make_unique<WatermelondbPackageTurboModuleFactoryDelegate>();
+    }
 };
+
+
 } // namespace rnoh
